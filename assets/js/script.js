65f36818b22503bccbc8096b657519b0bd8bf492
Monika Jakobsson <nimrin81@gmail.com>
2022-03-23 21:14:02 +0000

    Add script of hidden or unhidden features on start page. Adjust start flag. Clean up html comments.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-//DOMContentLoaded
+//DOMContentLoaded then start page interaction
 document.addEventListener("DOMContentLoaded", function() {
+    let start = document.getElementById('start');
+    let username = document.getElementById('user');
 
+    //user.addEventListener(...);
+    start.addEventListener('click', startGame() );
 }
 
 function startGame(){
-
+    document.getElementsByClassName('hidden').style.color = "black";
 }
 
 function quizQuestions() {
