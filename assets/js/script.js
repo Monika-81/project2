d0e101e5f9f4b9ef68a2ecb2b12092648f8f1c86
Monika Jakobsson <nimrin81@gmail.com>
2022-03-23 15:42:59 +0000

    Add function structure to script. Adjust styles for visual. Add next level div to html.

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+//DOMContentLoaded
+
+function startGame(){
+
+}
+
+function quizQuestions() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateScore() {
+
+}
+
+function nextQuestion() {
+
+}
+
+function nextLevel() {
+
+}
+
+function gameOver() {
+
+}
+
+function highscore() {
+
+}
+
+function playAgain() {
+    
+}
