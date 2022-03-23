902405e118f7d5a9add25472ff0bc6eefbf8afcc
Monika Jakobsson <nimrin81@gmail.com>
2022-03-23 23:13:47 +0000

    Add first question to script. Adjust game introduction text, add style to game area. Remove footer.

---
+++ assets/js/quiz.js
@@ -0,0 +1,34 @@
+/**
+ * Quiz questions
+ * Flag + 4 questions
+ * Flag source from https://flagpedia.net/download/api
+ */
+
+//Easy level questions, 30 in total but starts with 5 totest the code
+const easyQuestions [ {
+  question: 
+    <img src="https://flagcdn.com/80x60/cn.png" srcset="https://flagcdn.com/160x120/cn.png 2x,
+    https://flagcdn.com/240x180/cn.png 3x" width="80" height="60" alt="China"></img>,
+ 
+  answers: [
+    { text: "China", correct: true },
+    { text: "Italy", correct: false },
+    { text: "India", correct: false },
+    { text: "Peru", correct: false },
+},
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+{},
+
+
+
+
+
+
+]
